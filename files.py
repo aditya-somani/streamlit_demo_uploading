30ffaf010084be2b1479a53c5d7a380037cf2b42
Aditya Somani <137697163+aditya-somani@users.noreply.github.com>
2024-05-13 16:27:36 +0530

    Add files via upload

---
+++ files.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+st.subheader("Files")
+
+df=st.file_uploader("Upload your file ",type=["csv","xlsx"])
+if df is not None:
+    df = pd.read_csv(df)
+    if df is not None:
+        st.table(df.head())
+
+st.subheader("Dealing with images")
+st.image("/Users/H.P/Desktop/Python/Learning/Libraries/Streamlit/Ref/img.png")
+
+
+st.subheader("Dealing with images while uploading ")
+img=st.file_uploader("Upload your image ",type=["jpg","png","jpeg"])
+if img is not None:
+    st.image("/Users/H.P/Desktop/Python/Learning/Libraries/Streamlit/Ref/img.png")
+
+st.subheader("Video Uploader")
+vid=st.file_uploader("Upload your video ",type=["mp4","mkv"])
+if vid is not None:
+    st.video(vid,start_time=0)
+
+#This is an "raw String" using which i don't have to modify th epth "\" to "/"
+#As windows and unix based system handle path separators differently
+st.subheader("Using raw string to upload file ")
+st.video(r"C:\Users\H.P\Desktop\Python\Learning\Libraries\Streamlit\Ref\Video_Test.mp4")
+
+st.subheader("Audio Files")
+audi=st.file_uploader("Upload your audio ",type=["wav","mp3"])
+if audi is not None:
+    st.audio(audi.read())
+
+
+
