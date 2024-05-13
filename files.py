523b6fba14abebeff0529ad85c8d8ae29b661606
Aditya Somani <137697163+aditya-somani@users.noreply.github.com>
2024-05-13 16:39:35 +0530

    Update files.py

--- files.py
+++ files.py
@@ -9,13 +9,13 @@ if df is not None:
         st.table(df.head())
 
 st.subheader("Dealing with images")
-st.image("/Users/H.P/Desktop/Python/Learning/Libraries/Streamlit/Ref/img.png")
+st.image("img.png")
 
 
 st.subheader("Dealing with images while uploading ")
 img=st.file_uploader("Upload your image ",type=["jpg","png","jpeg"])
 if img is not None:
-    st.image("/Users/H.P/Desktop/Python/Learning/Libraries/Streamlit/Ref/img.png")
+    st.image(img)
 
 st.subheader("Video Uploader")
 vid=st.file_uploader("Upload your video ",type=["mp4","mkv"])
@@ -25,7 +25,7 @@ if vid is not None:
 #This is an "raw String" using which i don't have to modify th epth "\" to "/"
 #As windows and unix based system handle path separators differently
 st.subheader("Using raw string to upload file ")
-st.video(r"C:\Users\H.P\Desktop\Python\Learning\Libraries\Streamlit\Ref\Video_Test.mp4")
+st.video(Video_Test.mp4)
 
 st.subheader("Audio Files")
 audi=st.file_uploader("Upload your audio ",type=["wav","mp3"])
